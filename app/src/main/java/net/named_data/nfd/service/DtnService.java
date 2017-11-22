af625ac353d151ab436e5ff42954abe4603ca85b
Susana Pérez <susana.perezsanchez@tecnalia.com>
2017-11-22 11:36:59 +0100

    Running version with seamless neighbour list retrieving from IBR-DTN

--- app/src/main/java/net/named_data/nfd/service/DtnService.java
+++ app/src/main/java/net/named_data/nfd/service/DtnService.java
@@ -2,13 +2,20 @@ package net.named_data.nfd.service;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Binder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.os.SystemClock;
 
 // IBR-DTN LIBS
 import com.intel.jndn.management.ManagementException;
+
+import android.os.Handler;
+import android.os.Messenger;
+import android.os.Message;
 
 import de.tubs.ibr.dtn.api.BundleID;
 import de.tubs.ibr.dtn.api.DTNClient;
@@ -23,9 +30,14 @@ import de.tubs.ibr.dtn.api.SessionDestroyedException;
 import net.named_data.jndn.Name;
 import net.named_data.jndn_xx.util.FaceUri;
 import net.named_data.nfd.utils.DtnExtendedDataHandler;
+import net.named_data.nfd.utils.G;
 import net.named_data.nfd.utils.NfdcHelper;
+import net.named_data.nfd.utils.PermanentFaceUriAndRouteManager;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import de.tubs.ibr.dtn.api.SingletonEndpoint;
 
@@ -99,6 +111,8 @@ public class DtnService extends DTNIntentService {
         Registration reg = new Registration(IBRDTN_Affix);
         try {
             initialize(reg);
+            // invoke the dtn permanent uri creation and /ndn prefix registration
+            createPermanentFaceUriAndRoute();
         } catch (ServiceNotAvailableException e) {
             Log.i(TAG, "Service not available");
         }
@@ -126,6 +140,113 @@ public class DtnService extends DTNIntentService {
 
     }
 
+    // Permanent FACES and Routes are created here using Permanent...ger.java and NfdcHelper.java
+    private void createPermanentFaceUriAndRoute() {
+        final long checkInterval = 1000;
+        if (m_isDtnStarted) {
+            G.Log(TAG, "createPermanentFaceUriAndRoute: NFD is running, start executing task.");
+
+            final Handler periodic_handler = new Handler ();
+            periodic_handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    // Update Dtn Neighbours
+                    Log.i(TAG, "(tic) UpdateDtn Neighbours");
+                    Intent i = new Intent(getApplicationContext(), DtnService.class);
+                    i.setAction("UpdateN");
+                    i.putExtra("messenger", new Messenger(neighbourhandler));
+                    startService(i);
+
+
+                    boolean shouldUpd = false;
+                    if (currentNeighbours != null & oldNeigs == null) {
+                        shouldUpd = true;
+                    }
+                    if (currentNeighbours != null & oldNeigs != null) {
+                        if (oldNeigs.length != currentNeighbours.length) {
+                            shouldUpd = true;
+                        } else {
+                            for (int j = 0; j < oldNeigs.length; j++) {
+                                if (oldNeigs[j] == null || currentNeighbours[j] == null) {
+                                    break;
+                                }
+                                if (!oldNeigs[j].equals(currentNeighbours[j])) {
+                                    shouldUpd = true;
+                                }
+                            }
+                        }
+                    }
+
+                    if (shouldUpd) {
+                        Log.i(TAG, "(tic) SHOULD UPDATE DTN FACES");
+                        ArrayList<Integer> faceId = new ArrayList<Integer>();
+                        new FaceCreateAsyncTask(currentNeighbours, getApplicationContext()).execute();
+                    }
+
+                    periodic_handler.postDelayed(this, 5000);
+                }
+            }, 5000);
+
+
+        } else {
+            G.Log(TAG, "createPermanentFaceUriAndRoute: NFD is not started yet, delay " + String.valueOf(checkInterval) + " ms.");
+            prefix_handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    createPermanentFaceUriAndRoute();
+                }
+            }, checkInterval);
+        }
+    }
+
+    /**
+     * Create all DTN permanent faces in the background
+     */
+    private static class FaceCreateAsyncTask extends AsyncTask<Void, Void, Integer[]> {
+        String [] neighbours;
+        Context context;
+
+        FaceCreateAsyncTask(String [] neighs, Context ctx) {
+            this.neighbours = neighs;
+            this.context = ctx;
+        }
+
+        @Override
+        protected Integer[]
+        doInBackground(Void... params) {
+            NfdcHelper nfdcHelper = new NfdcHelper();
+            ArrayList<Integer> faceId = new ArrayList<Integer>();
+            try {
+                G.Log(TAG, "Try to create dtn neighbour face");
+                //Set<String> permanentFace = PermanentFaceUriAndRouteManager.getPermanentFaceUris(this.context);
+                G.Log(TAG, "Permanent face list has " + this.neighbours.length + " item(s)");
+                for (String n : neighbours) {
+                    int id = nfdcHelper.faceCreate(n);
+                    PermanentFaceUriAndRouteManager.addPermanentFaceId(this.context, id);
+                    nfdcHelper.ribRegisterPrefix(new Name("/ndn"), id, 10, true, false);
+                    faceId.add(id);
+                    G.Log(TAG, "Create permanent face " + id + ": " + n + " with prefix /ndn registered");
+                }
+            } catch (Exception e) {
+                G.Log(TAG, "Error in DTN face creation: " + e.getMessage());
+            } finally {
+                nfdcHelper.shutdown();
+            }
+            return faceId.toArray(new Integer[faceId.size()]);
+        }
+    }
+
+
+    public String[] getNeighbours(){
+        List<String> neighbours = new ArrayList<String>();
+        for (Node node : this.getClient().getNeighbors()) {
+            if ("NODE_INTERNET".compareTo(node.type) != 0) {
+                neighbours.add(node.endpoint.toString());
+            }
+        }
+        return neighbours.toArray(new String[neighbours.size()]);
+    }
 
     /**
      * Filter the intent-action and do corresponding work
@@ -134,6 +255,7 @@ public class DtnService extends DTNIntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         String action = intent.getAction();
+        String[] Neighbours = null;
 
         // if a new bundle was received by IBR-DTN - intent created by DtnBroadcastReceiver
         if (de.tubs.ibr.dtn.Intent.RECEIVE.equals(action)) {
@@ -173,6 +295,27 @@ public class DtnService extends DTNIntentService {
                 Log.e(TAG, "session destroyed", e);
             }
         }
+
+        else if (action.equals("UpdateN")) {
+            //Log.i(TAG,"Update Neighbours");
+            Neighbours = getNeighbours();
+
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Messenger messenger = (Messenger) bundle.get("messenger");
+                Message msg = Message.obtain();
+
+                Bundle bun = new Bundle();
+                bun.putStringArray("Neighbour", Neighbours);
+                msg.setData(bun); //put the data here
+
+                try {
+                    messenger.send(msg);
+                } catch (RemoteException e) {
+                    Log.i(TAG, "error in sending neighbours");
+                }
+            }
+        }
     }
 
     /**
@@ -202,6 +345,7 @@ public class DtnService extends DTNIntentService {
     private DataHandler mDataHandler = new DtnExtendedDataHandler() {
         @Override
         protected void onMessage(BundleID id, byte[] data) {
+
 
             String source = id.getSource().toString();
 
@@ -246,6 +390,29 @@ public class DtnService extends DTNIntentService {
 
     }
 
+    final Handler neighbourhandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle reply = msg.getData();
+            String[] strs = reply.getStringArray("Neighbour");
+            if (strs.length > 1) {
+                oldNeigs = currentNeighbours;
+                currentNeighbours = new String[strs.length - 1];
+                currentNeighbours = Arrays.copyOfRange(strs, 1, strs.length - 1);
+            } else {
+                oldNeigs = currentNeighbours;
+                Log.i(TAG, "oldNeigs is null");
+                currentNeighbours = new String[strs.length];
+                currentNeighbours = Arrays.copyOfRange(strs, 0, strs.length);
+            }
+        }
 
+    };
+
+
+    private String[] currentNeighbours;
+    private String[] oldNeigs;
     private boolean m_isDtnStarted = false;
+    private Handler prefix_handler = new Handler();
+
 }
